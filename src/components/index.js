1486eae411d2b2a0cb0a143027250848d79f7a4b
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-12-04 12:22:43 +0100

    feat: add basic architecture

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as NavBar } from "./Navbar/NavBar";
+export { default as EmployeeCreationForm } from "./EmployeeCreationForm/EmployeeCreationForm";
+export { default as EmployeeListTable } from "./EmployeeListTable/EmployeeListTable";
