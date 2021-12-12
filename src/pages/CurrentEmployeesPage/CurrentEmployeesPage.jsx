d3866d96b7d2d6ecd1e99f8bde8e434954667780
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-12-12 22:55:19 +0100

    feat: add goToPage option on table with style

--- src/pages/CurrentEmployeesPage/CurrentEmployeesPage.jsx
+++ src/pages/CurrentEmployeesPage/CurrentEmployeesPage.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from "react";
 import { CurrentEmployeesTable } from "../../components";
 import { currentEmployeesTableHead } from "../../components/table/utils/currentEmployeesTableHead";
+import checkedLocalStorage from "../../utils/divers/handleLocalStorage";
+import { mockEmployees } from "../../utils/data/mockEmployees";
 
 /**
  * @name CurrentEmployeesPage
  * @returns {ReactElement}
  */
 export default function CurrentEmployeesPage() {
+  // As long as the app is using local storage, we check it again in case the user
+  // dumps their local storage while browsing the application...
   const employees = JSON.parse(localStorage.getItem("HRnetEmployeesSession"));
+  checkedLocalStorage(employees, mockEmployees);
 
   useEffect(() => {
     document.title = "Current employees";
